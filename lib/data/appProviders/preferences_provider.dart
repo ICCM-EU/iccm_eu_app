023dcbfe451a3c8d6921850a94defbb4aeefbc52
Hans-Jürgen Tappe <hj.tappe@web.de>
2024-10-26 22:53:40 +0200

    Check for changes in the data before applying changes.

--- lib/data/appProviders/preferences_provider.dart
+++ lib/data/appProviders/preferences_provider.dart
@@ -13,4 +13,35 @@ class PreferencesProvider {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_isDarkThemeKey, value);
   }
+
+  // ---------------------------------------------------------
+  static const String _cachedChecksumKey = '_cachedChecksum';
+
+  static Future<String> get cachedChecksum async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_cachedChecksumKey) ?? ''; // Default to empty
+  }
+
+  static Future<void> setCachedChecksum(String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_cachedChecksumKey, value);
+  }
+
+  // ---------------------------------------------------------
+  static const String _cacheLastUpdatedKey = '_cacheLastUpdated';
+
+  static Future<DateTime?> get cacheLastUpdated async {
+    final prefs = await SharedPreferences.getInstance();
+    final lastUpdatedMillis = prefs.getInt(_cacheLastUpdatedKey);
+    if (lastUpdatedMillis != null) {
+      return DateTime.fromMillisecondsSinceEpoch(lastUpdatedMillis);
+    } else {
+      return null;
+    }
+  }
+
+  static Future<void> setLastUpdated(DateTime? lastUpdated) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_cacheLastUpdatedKey, lastUpdated!.millisecondsSinceEpoch);
+  }
 }
--- lib/data/dataProviders/gsheets_provider.dart
+++ lib/data/dataProviders/gsheets_provider.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'dart:core';
 
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gsheets/gsheets.dart';
+import 'package:iccm_eu_app/data/appProviders/preferences_provider.dart';
 import 'package:iccm_eu_app/data/dataProviders/events_provider.dart';
 import 'package:iccm_eu_app/data/dataProviders/rooms_provider.dart';
 import 'package:iccm_eu_app/data/dataProviders/speakers_provider.dart';
@@ -19,7 +22,6 @@ import 'error_provider.dart';
 class GsheetsProvider with ChangeNotifier {
   final String _sheetID =
       '1dFLWrcbI1AltIvVCEjBx9I3I3d0ToGN2FmzcFuAYsZE';
-  DateTime _lastFetchTime = DateTime.now().subtract(const Duration(days: 365));
   bool _isFetchingData = false;
 
   Future<String> _loadCredentials(ErrorProvider errorProvider) async {
@@ -30,6 +32,37 @@ class GsheetsProvider with ChangeNotifier {
       errorProvider.setErrorSignal(ErrorSignal('Error: $e'));
       rethrow; // Re-throw the exception to handle it elsewhere if needed
     }
+  }
+
+  String _generateChecksum(Map<String, List<Map<String, String>>> data) {
+    final jsonString = jsonEncode(data); // Convert data to JSON string
+    final bytes = utf8.encode(jsonString); // Encode string to bytes
+    final digest = sha256.convert(bytes); // Calculate SHA-256 hash
+    return digest.toString(); // Return checksum as a string
+  }
+
+  int _countDataObjects(Map<String, List<Map<String, String>>> data) {
+    int totalObjects = 0;
+
+    // Count the map itself
+    // totalObjects++;
+
+    // Iterate through keys and values
+    data.forEach((key, value) {
+      // totalObjects++; // Count the key (String)
+      // totalObjects++; // Count the value (List)
+
+      // Iterate through list of maps
+      // totalObjects += value.length; // Count each map in the list
+
+      // Iterate through map entries
+      for (var map in value) {
+        totalObjects += map.length; // Count each key in the map
+        // totalObjects += map.length; // Count each value in the map
+      }
+    });
+
+    return totalObjects;
   }
 
   Future<Map<String, List<Map<String, String>>>> _readWorksheets(
@@ -50,15 +83,7 @@ class GsheetsProvider with ChangeNotifier {
           throw Exception('Worksheet "$worksheetTitle" not found.');
         } else {
           final rows = await worksheet.values.map.allRows() ?? [];
-          // final headers = rows[0].values.toList();
-          // // Remove header row after extracting it.
-          // rows.removeAt(0);
           data[worksheetTitle] = rows;
-          //.map((row) {
-          //   // Get the values of the map as a list
-          //   final rowData = row.values.toList();
-          //   return _rowToMap(rowData, headers);
-          // }).cast<Map<String, dynamic>>().toList();
         }
       }
     } catch (e, stackTrace) {
@@ -67,107 +92,113 @@ class GsheetsProvider with ChangeNotifier {
       final fileName = match?.group(2) ?? 'unknown';
       final lineNumber = match?.group(3) ?? 'unknown';
       errorProvider.setErrorSignal(ErrorSignal('Error ($fileName:$lineNumber): $e'));
-      // rethrow; // Re-throw the exception to handle it elsewhere if needed
     }
 
     return data;
   }
 
-  // Map<String, dynamic> _rowToMap(
-  //     List<dynamic> row,
-  //     List<String> headers
-  //     ) {
-  //   final map = <String, dynamic>{};
-  //   for (var i = 0; i < headers.length; i++) {
-  //     map[headers[i]] = row[i];
-  //   }
-  //   return map;
-  // }
-
   Future<void> fetchData(
       BuildContext context,
       {bool force = false}
       ) async {
+    if (_isFetchingData) {
+      return;
+    }
+
     DateTime now = DateTime.now();
-
-    // Unblock running again by setting _lastFetchTime to null on force
-    if (force) {
-      _lastFetchTime = DateTime.now().subtract(const Duration(days: 365));
+    DateTime? lastUpdated = await PreferencesProvider.cacheLastUpdated;
+    if (force ||
+        (lastUpdated != null && lastUpdated.isAfter(now.subtract(
+            const Duration(minutes: 5))))
+    ) {
+      return;
     }
-    // Avoid running multiple times in parallel by setting the date early
-    if (now.difference(_lastFetchTime) > const Duration(minutes: 5)) {
-      _lastFetchTime = now;
-      ErrorProvider errorProvider = Provider.of<ErrorProvider>(
-          context, listen: false);
-      EventsProvider eventsProvider = Provider.of<EventsProvider>(
-          context, listen: false);
-      RoomsProvider roomsProvider = Provider.of<RoomsProvider>(
-          context, listen: false);
-      SpeakersProvider speakersProvider = Provider.of<SpeakersProvider>(
-          context, listen: false);
-      TracksProvider tracksProvider = Provider.of<TracksProvider>(
-          context, listen: false);
-      final List<ProviderData> providers = [
-        eventsProvider,
-        roomsProvider,
-        speakersProvider,
-        tracksProvider,
-      ];
-      final List<String> worksheetTitles = providers.map(
-              (provider) => provider.worksheetTitle).toList();
-      if (_isFetchingData) {
-        return;
-      }
-      _isFetchingData = true;
-      final data = await _readWorksheets(worksheetTitles, context).then((data) {
-        _isFetchingData = false;
-        return data;
-      });
 
-      for (final provider in providers) {
-        final worksheetTitle = provider.worksheetTitle;
-        if (provider == eventsProvider) {
-          if (data.containsKey(worksheetTitle)) {
-            provider.clear();
-            for (final itemData in data[worksheetTitle]!) { // Iterate through room data
-              try {
-                provider.add(EventData.fromItemData(itemData)); // Add the RoomData object to the list
-              } catch (e) {
-                // Set to null to open the request again.
-                _lastFetchTime = DateTime.now().subtract(const Duration(days: 365));
-                errorProvider.setErrorSignal(ErrorSignal('Error: $e'));
-              }
-            }
-          }
-        }
+    // FIXME: Don't use 'BuildContext's across async gaps.
+    ErrorProvider errorProvider = Provider.of<ErrorProvider>(
+        context, listen: false);
+    EventsProvider eventsProvider = Provider.of<EventsProvider>(
+        context, listen: false);
+    RoomsProvider roomsProvider = Provider.of<RoomsProvider>(
+        context, listen: false);
+    SpeakersProvider speakersProvider = Provider.of<SpeakersProvider>(
+        context, listen: false);
+    TracksProvider tracksProvider = Provider.of<TracksProvider>(
+        context, listen: false);
+    final List<ProviderData> providers = [
+      eventsProvider,
+      roomsProvider,
+      speakersProvider,
+      tracksProvider,
+    ];
+    final List<String> worksheetTitles = providers.map(
+            (provider) => provider.worksheetTitle).toList();
 
-        if (provider == roomsProvider) {
-          if (data.containsKey(worksheetTitle)) {
-            provider.clear();
-            for (final itemData in data[worksheetTitle]!) { // Iterate through room data
-              provider.add(RoomData.fromItemData(itemData)); // Add the RoomData object to the list
-            }
-          }
-        }
+    _isFetchingData = true;
+    final data = await _readWorksheets(worksheetTitles, context);
 
-        if (provider == speakersProvider) {
-          if (data.containsKey(worksheetTitle)) {
-            provider.clear();
-            for (final itemData in data[worksheetTitle]!) { // Iterate through room data
-              provider.add(SpeakerData.fromItemData(itemData)); // Add the RoomData object to the list
-            }
-          }
-        }
+    // Terminate if data is empty
+    if (_countDataObjects(data) == 0) {
+      return;
+    }
 
-        if (provider == tracksProvider) {
-          if (data.containsKey(worksheetTitle)) {
-            provider.clear();
-            for (final itemData in data[worksheetTitle]!) { // Iterate through room data
-              provider.add(TrackData.fromItemData(itemData)); // Add the RoomData object to the list
+    String cachedChecksum = await PreferencesProvider.cachedChecksum;
+    String dataChecksum = _generateChecksum(data);
+
+    if (lastUpdated != null && cachedChecksum == dataChecksum) {
+      return;
+    }
+    await PreferencesProvider.setCachedChecksum(dataChecksum);
+    await PreferencesProvider.setLastUpdated(now);
+
+    for (final provider in providers) {
+      final worksheetTitle = provider.worksheetTitle;
+      if (provider == eventsProvider) {
+        if (data.containsKey(worksheetTitle)) {
+          provider.clear();
+          for (final itemData in data[worksheetTitle]!) { // Iterate through room data
+            try {
+              provider.add(EventData.fromItemData(
+                  itemData)); // Add the RoomData object to the list
+            } catch (e) {
+              // Set to null to open the request again.
+              await PreferencesProvider.setLastUpdated(null);
+              errorProvider.setErrorSignal(ErrorSignal('Error: $e'));
             }
           }
         }
       }
+
+      if (provider == roomsProvider) {
+        if (data.containsKey(worksheetTitle)) {
+          provider.clear();
+          for (final itemData in data[worksheetTitle]!) { // Iterate through room data
+            provider.add(RoomData.fromItemData(
+                itemData)); // Add the RoomData object to the list
+          }
+        }
+      }
+
+      if (provider == speakersProvider) {
+        if (data.containsKey(worksheetTitle)) {
+          provider.clear();
+          for (final itemData in data[worksheetTitle]!) { // Iterate through room data
+            provider.add(SpeakerData.fromItemData(
+                itemData)); // Add the RoomData object to the list
+          }
+        }
+      }
+
+      if (provider == tracksProvider) {
+        if (data.containsKey(worksheetTitle)) {
+          provider.clear();
+          for (final itemData in data[worksheetTitle]!) { // Iterate through room data
+            provider.add(TrackData.fromItemData(
+                itemData)); // Add the RoomData object to the list
+          }
+        }
+      }
     }
+    _isFetchingData = false;
   }
 }
