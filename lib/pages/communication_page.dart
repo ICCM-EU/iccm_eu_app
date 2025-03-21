43da5fcc853ce59f566b0b54aa71f0d54768c684
Hans-Jürgen Tappe <hj.tappe@web.de>
2025-03-21 09:48:07 +0100

    #34: Fix the display of the communication link list.

--- lib/pages/communication_page.dart
+++ lib/pages/communication_page.dart
@@ -16,36 +16,45 @@ class CommunicationPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Communication'),
       ),
-      body: SliverToBoxAdapter(
-        child: Consumer<CommunicationProvider>(
-          builder: (context, itemProvider, child) {
-            final itemList = itemProvider.items();
-            if (itemList.isEmpty) {
-              return const Center(
-                child: Text('Loading dynamic content...'),
-              );
-            }
-            return ListView.builder(
-              shrinkWrap: true,
-              physics: ClampingScrollPhysics(),
-              itemCount: itemList.length,
-              itemBuilder: (context, index) {
-                CommunicationData item = itemList[index];
-                if (item.title.isEmpty ||
-                    !item.url.startsWith('https://')) {
-                  return SizedBox.shrink();
-                }
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: UrlButton(
-                    title: item.title,
-                    url: item.url,
-                  ),
-                );
-              },
+      body: Consumer<CommunicationProvider>(
+        builder: (context, itemProvider, child) {
+          final itemList = itemProvider.items();
+          if (itemList.isEmpty) {
+            return const Center(
+              child: Text('Loading dynamic content...'),
             );
-          },
-        ),
+          }
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            itemCount: itemList.length,
+            itemBuilder: (context, index) {
+              CommunicationData item = itemList[index];
+              if (item.title.isEmpty ||
+                  !item.url.startsWith('https://')) {
+                return SizedBox.shrink();
+              }
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: UrlButton(
+                      title: item.title,
+                      url: item.url,
+                    ),
+                  ),
+                  Text(
+                    item.url,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .bodySmall,
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
