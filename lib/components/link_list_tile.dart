6ddbf0859e82fbde09effda97c65f2a10a254f5d
Hans-Jürgen Tappe <hj.tappe@web.de>
2025-03-21 20:18:40 +0100

    #34: Remove favicon parsing: CORS restrictions

--- lib/components/link_list_tile.dart
+++ lib/components/link_list_tile.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:iccm_eu_app/data/model/communication_data.dart';
 import 'package:iccm_eu_app/utils/url_functions.dart';
@@ -13,60 +12,7 @@ class LinkListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    UrlFunctions.getFaviconUrl(item.url).then((websiteIconUrl) {
-      if (websiteIconUrl != null) {
-      } else {
-      }
-    });
     return ListTile(
-      leading: FutureBuilder<String?>(
-        future: UrlFunctions.getFaviconUrl(item.url),
-        builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // While waiting for the Future to complete, show a loading indicator
-            return const SizedBox(
-              width: 100,
-              height: 100,
-              child: FittedBox(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            // If there's an error, show an error icon
-            return const SizedBox(
-              width: 100,
-              height: 100,
-              child: FittedBox(
-                child: Icon(Icons.error),
-              ),
-            );
-          } else if (snapshot.hasData && snapshot.data != null) {
-            // If the Future completed successfully and has data, show the image
-            return CircleAvatar(
-              radius: 50.0,
-              child: SizedBox(
-                width: 100.0,
-                height: 100.0,
-                child: CachedNetworkImage(
-                  imageUrl: snapshot.data!,
-                  placeholder: (context, url) => const FittedBox(
-                    child: CircularProgressIndicator(),
-                  ),
-                  errorWidget: (context, url, error) => const FittedBox(
-                    child: Icon(Icons.error),
-                  ),
-                ),
-              ),
-            );
-          } else {
-            // If the Future completed successfully but has no data (null), show an empty box
-            return const SizedBox(
-              height: 100,
-              width: 100,
-            );
-          }
-        },
-      ),
       title: RichText(
         text: TextSpan(
           style: DefaultTextStyle
